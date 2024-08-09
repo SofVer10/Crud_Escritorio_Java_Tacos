fe9e3ae39e70079c7403a5a45fd1e389835d069d
Sofia <20230106@ricaldone.edu.sv>
2024-08-09 11:51:11 -0600

    Insertar datos ya esta pero falta

---
+++ src/controlador/ctrlMenu.java
@@ -0,0 +1,65 @@
+package controlador;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import modelo.Menu;
+import vista.frmMenu;
+
+public class ctrlMenu implements MouseListener, KeyListener{
+    
+    private Menu modeloMenu;
+    private frmMenu vistaMenu;
+    
+    //constructor
+    
+    public ctrlMenu(Menu ModeloMenu, frmMenu frmMenu){
+        this.modeloMenu = ModeloMenu;
+        this.vistaMenu = frmMenu;
+        
+        frmMenu.btnGuardar.addMouseListener(this);
+    }
+    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/modelo/Menu.java
@@ -0,0 +1,64 @@
+package modelo;
+
+import java.sql.*;
+import java.util.UUID;
+
+public class Menu {
+    String UUID_menu;
+    String nombre;
+    double precio;
+    String ingredientes;
+
+    public String getUUID_menu() {
+        return UUID_menu;
+    }
+
+    public void setUUID_menu(String UUID_menu) {
+        this.UUID_menu = UUID_menu;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+    
+    //metodos
+      public void Guardar() {
+        //Creamos una variable igual a ejecutar el método de la clase de conexión
+        Connection conexion = ClaseConexion.getConexion();
+        try {
+            //Creamos el PreparedStatement que ejecutará la Query
+            PreparedStatement addMenu = conexion.prepareStatement("INSERT INTO tbCRUD(UUID, Nombre, Edad, Especialidad) VALUES (?, ?, ?, ?)");
+            //Establecer valores de la consulta SQL
+            addMenu.setString(1, UUID.randomUUID().toString());
+            addMenu.setString(2, getNombre());
+            addMenu.setDouble(3, getPrecio());
+            addMenu.setString(4, getIngredientes());
+ 
+            //Ejecutar la consulta
+            addMenu.executeUpdate();
+ 
+        } catch (SQLException ex) {
+            System.out.println("este es el error en el modelo:metodo guardar " + ex);
+        }
+    }
+}
